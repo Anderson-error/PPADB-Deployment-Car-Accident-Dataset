69a5b12d6dd483742cf3251e6f408627690bd3af
Andre Firman <115331048+Anderson-error@users.noreply.github.com>
2024-04-18 18:13:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ df.columns = [col.replace("-", "").replace("+", "").replace(" ", "_") for col in
 # y = df['Accident_Severity']
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train the model
 # model = RandomForestClassifier(n_estimators=100, random_state=42)
