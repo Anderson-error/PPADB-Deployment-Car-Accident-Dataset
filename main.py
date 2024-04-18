3be2620ef364559f7c41d89a0a8b2b8d98b81480
Andre Firman <115331048+Anderson-error@users.noreply.github.com>
2024-04-18 18:17:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,41 +9,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('DataCleanedUp.csv')
 df.columns = [col.replace("-", "").replace("+", "").replace(" ", "_") for col in df.columns]
 
-# Pilih kolom-kolom yang akan digunakan sebagai fitur-fitur
-# features = [
-#     'Latitude', 'Longitude', 'Number_of_Casualties', 'Number_of_Vehicles', 
-#     'Speed_limit', 'Day_of_Week_Friday', 'Day_of_Week_Monday', 'Day_of_Week_Saturday', 
-#     'Day_of_Week_Sunday', 'Day_of_Week_Thursday', 'Day_of_Week_Tuesday', 'Day_of_Week_Wednesday', 
-#     'Light_Conditions_Darkness__lighting_unknown', 'Light_Conditions_Darkness__lights_lit', 
-#     'Light_Conditions_Darkness__lights_unlit', 'Light_Conditions_Darkness__no_lighting', 
-#     'Light_Conditions_Daylight', 'Road_Type_Dual_carriageway', 'Road_Type_One_way_street', 
-#     'Road_Type_Roundabout', 'Road_Type_Single_carriageway', 'Road_Type_Slip_road', 
-#     'Urban_or_Rural_Area_Rural', 'Urban_or_Rural_Area_Urban', 
-#     'Weather_Conditions_Fine__high_winds', 'Weather_Conditions_Fine_no_high_winds', 
-#     'Weather_Conditions_Fog_or_mist', 'Weather_Conditions_Other', 
-#     'Weather_Conditions_Raining__high_winds', 'Weather_Conditions_Raining_no_high_winds', 
-#     'Weather_Conditions_Snowing__high_winds', 'Weather_Conditions_Snowing_no_high_winds'
-# ]
-
-# Split the data into features and target variable
-# X = df[features]
-# y = df['Accident_Severity']
-
-# Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train the model
-# model = RandomForestClassifier(n_estimators=100, random_state=42)
-# model.fit(X_train, y_train)
-
-# Save the trained model as a pickle file
-# with open('CarAccidentModel.pkl', 'wb') as f:
-#     pickle1.dump(model, f)
-
-# print("Model saved successfully as 'CarAccidentModel.pkl'")
-
-
-with open('apple_model.pkl', 'rb') as f:
+with open('CarAccidentModel.pkl', 'rb') as f:
         car_model = pickle1.load(f)
 
 def main():
